b7ea8e3dd749de00c2d309e5c19cbed3ce346f08
Cenk Alti <cenkalti@gmail.com>
2015-12-26 02:31:01 +0200

    upgrade schema

--- yap.py
+++ yap.py
@@ -13,43 +13,46 @@ import os.path
 import sqlite3
 
 conn = None
-c = None
 
 
-def create_db():
-    c.execute('''create table if not exists todo (
+def setup_db():
+    current_version = conn.execute("pragma user_version").fetchone()[0]
+    statements = [
+        '''create table if not exists todo (
         id integer primary key,
         title text not null,
-        done boolean not null default 0)''')
+        done boolean not null default 0)''',
+    ]
+    for statement in statements[current_version:]:
+        conn.execute(statement)
+        current_version += 1
+        conn.execute("pragma user_version = %d" % current_version)
 
 
 def cmd_add(args):
     title = " ".join(args)
-    c.execute("insert into todo(title) values(?)", (title,))
+    conn.execute("insert into todo(title) values(?)", (title, ))
 
 
 def cmd_list(args):
-    for row in c.execute("select id, title, done from todo"):
+    for row in conn.execute("select id, title, done from todo"):
         check = "✓" if row[2] == 1 else " "
         print check, row[0], row[1]
 
 
 def cmd_done(args):
     todo_id = int(args[0])
-    c.execute("update todo set done=1 where id=?", (todo_id,))
+    conn.execute("update todo set done=1 where id=?", (todo_id, ))
 
 
 def cmd_undone(args):
     todo_id = int(args[0])
-    c.execute("update todo set done=0 where id=?", (todo_id,))
+    conn.execute("update todo set done=0 where id=?", (todo_id,))
 
 
-def main():
-    global conn, c
+if __name__ == "__main__":
     conn = sqlite3.connect(os.path.expanduser("~/.yap.db"))
-    c = conn.cursor()
-    create_db()
-
+    setup_db()
     cmd, args = sys.argv[1], sys.argv[2:]
     {
         "add": cmd_add,
@@ -57,10 +60,5 @@ def main():
         "done": cmd_done,
         "undone": cmd_undone,
     }[cmd](args)
-
     conn.commit()
     conn.close()
-
-
-if __name__ == "__main__":
-    main()

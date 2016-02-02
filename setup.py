1322d1a64cde317cac1997b62dede0f2596d42cc
Cenk Alti <cenkalti@gmail.com>
2016-02-03 01:47:34 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,49 @@
+# coding=utf-8
+import os
+import re
+from setuptools import setup
+
+
+def read(*fname):
+    with open(os.path.join(os.path.dirname(__file__), *fname)) as f:
+        return f.read()
+
+
+def get_version():
+    for line in read('yap', '__init__.py').splitlines():
+        m = re.match(r"""__version__\s*=\s*['"](.*)['"]""", line)
+        if m:
+            return m.groups()[0].strip()
+    raise Exception('Cannot find version')
+
+
+setup(
+    name='yap',
+    version=get_version(),
+    author=u'Cenk Altı',
+    author_email='cenkalti@gmail.com',
+    keywords='todo task',
+    url='https://github.com/cenkalti/yap',
+    packages=['yap'],
+    install_requires=[
+        'SQLAlchemy>=1.0,<2',
+        'tabulate>=0.7,<2',
+    ],
+    description='Command line todo app',
+    zip_safe=True,
+    entry_points={
+        'console_scripts': [
+            'yap = yap.__main__:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities',
+    ],
+)
--- yap/__main__.py
+++ yap/__main__.py
@@ -1,7 +1,11 @@
 import yap.db
 import yap.commands
 
-if __name__ == '__main__':
+
+def main():
     yap.db.setup()
     yap.db.do_maintenance()
     yap.commands.parse_args()
+
+if __name__ == '__main__':
+    main()

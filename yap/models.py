fc1cd25a1cee8697ef4f2fef0995e4742cf09e59
Cenk Alti <cenkalti@gmail.com>
2016-02-07 01:14:42 +0200

    fix color

--- yap/models.py
+++ yap/models.py
@@ -120,8 +120,8 @@ def str_datetime(dt):
 
 
 def red(s):
-    return "\033[91m {}\033[00m".format(s)
+    return "\033[91m{}\033[00m".format(s)
 
 
 def yellow(s):
-    return "\033[93m {}\033[00m".format(s)
+    return "\033[93m{}\033[00m".format(s)

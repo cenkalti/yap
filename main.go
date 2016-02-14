6a47486936a3273a4b6222c35ebfd3297488a825
Cenk Alti <cenkalti@gmail.com>
2016-02-15 01:16:22 +0200

    cli

--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"os"
+
+	"github.com/codegangsta/cli"
+)
+
 func main() {
-	println("ok")
+	app := cli.NewApp()
+	app.Name = "yap"
+	app.Usage = "todo app"
+	app.Action = func(c *cli.Context) {
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "add",
+			Aliases: []string{"a"},
+			Usage:   "add new task",
+			Action: func(c *cli.Context) {
+			},
+		},
+		{
+			Name:    "list",
+			Aliases: []string{"l"},
+			Usage:   "list tasks",
+			Action: func(c *cli.Context) {
+			},
+		},
+		{
+			Name:    "complete",
+			Aliases: []string{"c"},
+			Usage:   "complete a task",
+			Action: func(c *cli.Context) {
+			},
+		},
+	}
+
+	app.Run(os.Args)
 }
